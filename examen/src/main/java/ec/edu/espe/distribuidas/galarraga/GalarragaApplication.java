7eaea8a40b1679c4d095fb0e77d467850166aa19
María Fernanda Galárraga Molina <43796434+FernandaGalarraga@users.noreply.github.com>
2021-08-09 19:46:10 -0500

    crear proyecto

---
+++ examen/src/main/java/ec/edu/espe/distribuidas/galarraga/GalarragaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.galarraga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalarragaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalarragaApplication.class, args);
+	}
+
+}
---
+++ examen/src/test/java/ec/edu/espe/distribuidas/galarraga/GalarragaApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.distribuidas.galarraga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalarragaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
